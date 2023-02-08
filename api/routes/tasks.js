d8bac85725cfb56c16af8021bfc15237807b4234
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2023-02-08 19:19:45 +0100

    Add Parser and CORS

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,56 +1,55 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   res.status(200).json({
-    message: "here we handle GET request to /tasks",
+    message: 'here we handle GET request to /tasks',
   });
 });
 
-router.post("/", (req, res, next) => {
+router.post('/', (req, res, next) => {
   res.status(200).json({
-    message: "Here we handle POST request to /tasks",
+    message: 'Here we handle POST request to /tasks',
   });
 });
 
-router.get("/:taskId", (req, res, next) => {
+router.get('/:taskId', (req, res, next) => {
   const id = req.params.taskId;
-  if (id === "special") {
+  if (id === 'special') {
     res.status(200).json({
-      message: "Now you see the content with special ID",
+      message: 'Now you see the content with special ID',
       id: id,
     });
   }else{
     res.status(200).json({
-      message: "Now you passed some ID"
+      message: 'Now you passed some ID'
     });
   }
 });
-
-router.patch("/:taskId", (req, res, next) => {
+router.patch('/:taskId', (req, res, next) => {
   const id = req.params.taskId;
-  if(id === "special"){
+  if(id === 'special'){
     res.status(200).json({
-      message: "Updated product",
+      message: 'Updated product',
       id: id
     });
   }else{
     res.status(200).json({
-      message: "None product has been updated"
+      message: 'None product has been updated'
     });
   }
 });
 
-router.delete("/:taskId", (req, res, next) => {
+router.delete('/:taskId', (req, res, next) => {
   const id = req.params.taskId;
-  if(id === "special"){
+  if(id === 'special'){
     res.status(200).json({
-      message: "Deleted product",
+      message: 'Deleted product',
       id: id
     });
   }else{
     res.status(200).json({
-      message: "None product has been deleted"
+      message: 'None product has been deleted'
     });
   }
 });
--- app.js
+++ app.js
@@ -1,30 +1,49 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
 var app = express();
-const logger = require("morgan");
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
-const taskRoutes = require("./api/routes/tasks");
-//logger
-app.use(morgan("dev"));
+//CORS handling for RESTful API - communication from another PORTS
+//catch all requests and pass CORS Headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); //access to any origin
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin. X-Requested-With, Content-Type, Accept, Authorization'
+  );//set accepted Header Types
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    return res.status(200).json({});//return empty JSON with HTTP status 'OK'
+  }//if request method is 'OPTION' we set which we allow 
+
+  next();//PASS Incoming Request to the next Route
+});
+const taskRoutes = require('./api/routes/tasks');
+
+//logger(Morgan Package)
+app.use(logger('dev'));
+
+app.use(bodyParser.urlencoded({extended: false}));//only simple bodies
+app.use(bodyParser.json());//want json to be used.
 
 //use() - middleware stack
-app.use("/tasks", taskRoutes);
+app.use('/tasks', taskRoutes);
 
 //handling ERRORS
 app.use((req, res, next) => {
-  const error = new Error("Not found");//Client can communicate but we cannot response on request
+  const error = new Error('Not found'); //Client can communicate but we cannot response on request
   error.status = 404;
   next(error); // forward this error
 });
 //above if cannot properly response on Request
 
 app.use((error, req, res, next) => {
- res.status(error.status || 500);
- res.json({
-  error: {
-    message: error.message,
-  }
- });
-});//above if some kind of server error was happen
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+}); //above if some kind of server error was happen
 
 module.exports = app;
