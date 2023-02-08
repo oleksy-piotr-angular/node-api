b032c184d3a7d2ed32154a06ce94fb3f0dc5c5fb
oleksy-piotr-angular <oleksy.piotr.angular@gmai.com>
2023-02-08 18:34:38 +0100

    Error handling

---
+++ api/routes/tasks.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "here we handle GET request to /tasks",
+  });
+});
+
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Here we handle POST request to /tasks",
+  });
+});
+
+router.get("/:taskId", (req, res, next) => {
+  const id = req.params.taskId;
+  if (id === "special") {
+    res.status(200).json({
+      message: "Now you see the content with special ID",
+      id: id,
+    });
+  }else{
+    res.status(200).json({
+      message: "Now you passed some ID"
+    });
+  }
+});
+
+router.patch("/:taskId", (req, res, next) => {
+  const id = req.params.taskId;
+  if(id === "special"){
+    res.status(200).json({
+      message: "Updated product",
+      id: id
+    });
+  }else{
+    res.status(200).json({
+      message: "None product has been updated"
+    });
+  }
+});
+
+router.delete("/:taskId", (req, res, next) => {
+  const id = req.params.taskId;
+  if(id === "special"){
+    res.status(200).json({
+      message: "Deleted product",
+      id: id
+    });
+  }else{
+    res.status(200).json({
+      message: "None product has been deleted"
+    });
+  }
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
 var app = express();
-//use() - middleware stack
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "It works",
-  });
-});
+const logger = require("morgan");
 
+const taskRoutes = require("./api/routes/tasks");
+//logger
+app.use(morgan("dev"));
+
+//use() - middleware stack
+app.use("/tasks", taskRoutes);
+
+//handling ERRORS
+app.use((req, res, next) => {
+  const error = new Error("Not found");//Client can communicate but we cannot response on request
+  error.status = 404;
+  next(error); // forward this error
+});
+//above if cannot properly response on Request
+
+app.use((error, req, res, next) => {
+ res.status(error.status || 500);
+ res.json({
+  error: {
+    message: error.message,
+  }
+ });
+});//above if some kind of server error was happen
 
 module.exports = app;
